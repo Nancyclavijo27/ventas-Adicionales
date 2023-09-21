8241024cc641f60e12ba6281a8d0acb70eefe4a0
Nancy Clavijo Varela <99740842+Nancyclavijo27@users.noreply.github.com>
2023-09-21 09:32:07 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router-dom";
+//import DallE from "./componets/DallE";
+
+const App = () => {
+  return (
+   
+     <h1>nancy</h1>
+   
+  );
+};
+
+export default App;
